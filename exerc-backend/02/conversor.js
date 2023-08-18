d0a282aa3a5c49b651201c66fe5232c1954cb408
Samuel Alves Pacheco <spalves.2003@gmail.com>
2023-08-18 16:29:50 -0300

    Conversor de graus

---
+++ exerc-backend/02/conversor.js
@@ -0,0 +1,8 @@
+// Valor em graus Fahrenheit
+const fahrenheit = 73; // Substitua pelo valor em Fahrenheit
+
+// Conversão para Celsius
+const celsius = (fahrenheit - 32) * (5/9);
+
+// Impressão do resultado
+console.log(`A temperatura em Celsius é: ${celsius.toFixed(0)} ºC`);
