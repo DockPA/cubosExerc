bc1b1ff0b9a3281abfc1d7e75965cca370ba1841
Samuel Alves Pacheco <spalves.2003@gmail.com>
2023-08-18 16:27:07 -0300

    Calculo de IMC

---
+++ exerc-backend/01/imc.js
@@ -0,0 +1,9 @@
+// Valores do peso e altura
+const peso = 76; // Substitua pelo peso da pessoa em kg
+const altura = 1.79; // Substitua pela altura da pessoa em metros
+
+// Cálculo do IMC
+const imc = peso / (altura ** 2);
+
+// Impressão do resultado
+console.log(`O IMC é: ${imc.toFixed(1)}`);
